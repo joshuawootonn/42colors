fea48ef833668cea2ccbfc0ef62805fee23b575c
Joshua Wootonn <jose56wonton@gmail.com>
2020-02-05 20:45:34 -0600

    .

---
+++ frontend/src/components/__tests__/dumb.test.tsx
@@ -0,0 +1,6 @@
+describe('this is a test for the ci pipeline', () => {
+    it('WHEN this test is run THEN it will pass', () => {
+        expect(1).toBe(1);
+        expect(1).toBe(1);
+    });
+});
