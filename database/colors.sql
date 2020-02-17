1fc300ec246a226f47181d9308b4ec9ecbd7abf8
Joshua Wootonn <jose56wonton@gmail.com>
2020-02-17 14:22:49 -0600

    task: create initial postgis schema

---
+++ database/colors.sql
@@ -0,0 +1,33 @@
+
+create table player
+(
+  player_id SERIAL PRIMARY KEY,
+  name character varying(20),
+
+  created_at timestamp NOT NULL DEFAULT NOW()
+);
+
+create table line 
+(
+  line_id SERIAL PRIMARY KEY,  
+  geom geometry(MultiLineString,4326) NOT NULL,
+  duration int NOT NULL DEFAULT 0,
+  brushColor character varying(20) NOT NULL,
+  brushWidth double precision NOT NULL,
+  
+  created_at timestamp NOT NULL DEFAULT NOW(),
+
+  player_id integer
+);
+
+
+-- connect line to user
+alter table line 
+    add constraint fk_line_player
+    foreign key (player_id) 
+    REFERENCES player (player_id);
+
+CREATE INDEX lines_geom_idx
+    ON line USING gist
+    (geom)
+    TABLESPACE pg_default;
