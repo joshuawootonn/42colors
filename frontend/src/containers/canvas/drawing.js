7de26c2f49447eadddd48489fce0de4b3f593ee5
Joshua Wootonn <jose56wonton@gmail.com>
2020-04-08 00:35:41 -0500

    wip: wow we don't need those null checks

--- frontend/src/containers/canvas/drawing.js
+++ frontend/src/containers/canvas/drawing.js
@@ -15,27 +15,23 @@ const TempCanvas = styled.canvas`
 `;
 
 const Drawing = ({ canvasSettings, ...props }) => {
-    const drawingCanvas = useRef(null);
-    const drawingContext = useRef(null);
-    const tempCanvas = useRef(null);
-    const tempContext = useRef(null);
+    const drawingCanvas = useRef();
+    const drawingContext = useRef();
+    const tempCanvas = useRef();
+    const tempContext = useRef();
 
     const { width, height } = useWindowSize();
     const [isPanning, mapPosition] = useMapPosition();
     const panOffset = useRef({ x: 0, y: 0 });
 
     const redraw = () => {
-        if (drawingCanvas.current && drawingContext.current) {
-            clear2(drawingContext.current, drawingCanvas.current.width, drawingCanvas.current.height);
-            drawLines(drawingContext.current, props.lines, mapPosition);
-        }
+        clear2(drawingContext.current, drawingCanvas.current.width, drawingCanvas.current.height);
+        drawLines(drawingContext.current, props.lines, mapPosition);
     };
 
     const reContext = () => {
-        if (drawingCanvas.current && tempCanvas.current) {
-            drawingContext.current = drawingCanvas.current.getContext('2d');
-            tempContext.current = tempCanvas.current.getContext('2d');
-        }
+        drawingContext.current = drawingCanvas.current.getContext('2d');
+        tempContext.current = tempCanvas.current.getContext('2d');
     };
 
     useEffect(() => {
@@ -47,33 +43,27 @@ const Drawing = ({ canvasSettings, ...props }) => {
     }, [props.lines]);
 
     useEffect(() => {
-        if (tempCanvas.current && tempContext.current && drawingCanvas.current) {
-            if (isPanning) {
-                copyFromCanvasToContext(tempContext.current, drawingCanvas.current, 0, 0);
-                panOffset.current = { ...mapPosition };
-            } else {
-                clear2(tempContext.current, tempCanvas.current.width, tempCanvas.current.height);
-            }
+        if (isPanning) {
+            copyFromCanvasToContext(tempContext.current, drawingCanvas.current, 0, 0);
+            panOffset.current = { ...mapPosition };
+        } else {
+            clear2(tempContext.current, tempCanvas.current.width, tempCanvas.current.height);
         }
     }, [isPanning]);
 
     useEffect(() => {
-        if (tempCanvas.current && drawingCanvas.current && drawingContext.current && panOffset.current) {
-            clear2(drawingContext.current, drawingCanvas.current.width, drawingCanvas.current.height);
-            copyFromCanvasToContext(
-                drawingContext.current,
-                tempCanvas.current,
-                panOffset.current.x - mapPosition.x,
-                panOffset.current.y - mapPosition.y
-            );
-        }
+        clear2(drawingContext.current, drawingCanvas.current.width, drawingCanvas.current.height);
+        copyFromCanvasToContext(
+            drawingContext.current,
+            tempCanvas.current,
+            panOffset.current.x - mapPosition.x,
+            panOffset.current.y - mapPosition.y
+        );
     }, [mapPosition]);
 
     useEffect(() => {
-        if (drawingCanvas.current && tempCanvas.current) {
-            resizeCanvas(drawingCanvas.current, width, height);
-            resizeCanvas(tempCanvas.current, width, height);
-        }
+        resizeCanvas(drawingCanvas.current, width, height);
+        resizeCanvas(tempCanvas.current, width, height);
         reContext();
         redraw();
     }, [width, height]);
