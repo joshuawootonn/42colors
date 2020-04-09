fb55f25782cc66f1ccb4e7f2de1183721b5c580c
Joshua Wootonn <jose56wonton@gmail.com>
2020-04-08 19:37:40 -0500

    feat: finish optimization on brush

--- frontend/src/helpers/canvas.helpers.tsx
+++ frontend/src/helpers/canvas.helpers.tsx
@@ -29,29 +29,25 @@ export const drawGrid = (context: CanvasRenderingContext2D, mapPosition: Point) 
     context.stroke();
 };
 
-export const drawBrush = (canvas: RefObject<HTMLCanvasElement>, pointer: Point, canvasSettings: CanvasSettings, isPanning: boolean) => {
-    if (canvas.current && canvas.current.getContext('2d')) {
-        const c = canvas.current.getContext('2d') as CanvasRenderingContext2D;
-
-        if (isPanning) {
-            const img = new Image();
-            img.src = pan;
-
-            c.clearRect(0, 0, c.canvas.width, c.canvas.height);
-            c.drawImage(img, pointer.x, pointer.y, 40, 40);
-            return;
-        }
-
+export const drawBrush = (context: CanvasRenderingContext2D, pointer: Point, canvasSettings: CanvasSettings, isPanning: boolean) => {
+    if (isPanning) {
         const img = new Image();
-        img.src = pen;
+        img.src = pan;
 
-        c.clearRect(0, 0, c.canvas.width, c.canvas.height);
-        c.beginPath();
-        c.fillStyle = canvasSettings.brushColor;
-        c.arc(pointer.x, pointer.y, canvasSettings.brushWidth, 0, Math.PI * 2, true);
-        c.fill();
-        c.drawImage(img, pointer.x, pointer.y, 40, 40);
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        context.drawImage(img, pointer.x, pointer.y, 40, 40);
+        return;
     }
+
+    const img = new Image();
+    img.src = pen;
+
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context.beginPath();
+    context.fillStyle = canvasSettings.brushColor;
+    context.arc(pointer.x, pointer.y, canvasSettings.brushWidth, 0, Math.PI * 2, true);
+    context.fill();
+    context.drawImage(img, pointer.x, pointer.y, 40, 40);
 };
 
 function midPointBtw(p1: Point, p2: Point) {
@@ -66,58 +62,6 @@ export const toRelative = (point: Point, mapPosition: Point): Point => {
 };
 export const toAbsolute = (point: Point, mapPosition: Point): Point => {
     return { x: point.x + mapPosition.x, y: point.y + mapPosition.y };
-};
-
-export const drawPoints = (
-    canvas: RefObject<HTMLCanvasElement>,
-    points: Point[],
-    mapPosition: Point,
-    brushColor: string,
-    brushWidth: number
-) => {
-    if (canvas.current && canvas.current.getContext('2d')) {
-        const c = canvas.current.getContext('2d') as any;
-        c.lineJoin = 'round';
-        c.lineCap = 'round';
-        c.strokeStyle = brushColor;
-        c.lineWidth = brushWidth * 2;
-
-        let p1 = toRelative(points[0], mapPosition);
-
-        if (points.length === 1) {
-            c.beginPath();
-            c.arc(points[0].x, points[0].y, 1, 0, 2 * Math.PI, true);
-            c.stroke();
-            return;
-        }
-
-        let p2 = toRelative(points[1], mapPosition);
-
-        c.moveTo(p2.x, p2.y);
-        c.beginPath();
-
-        for (let i = 1, len = points.length; i < len; i++) {
-            // we pick the point between pi+1 & pi+2 as the
-            // end point and p1 as our control point
-            const midPoint = midPointBtw(p1, p2);
-            c.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
-            p1 = toRelative(points[i], mapPosition);
-            i + 1 !== len && (p2 = toRelative(points[i + 1], mapPosition));
-        }
-        // Draw last line as a straight line while
-        // we wait for the next point to be able to calculate
-        // the bezier control point
-        c.lineTo(p1.x, p1.y);
-        c.stroke();
-    }
-};
-
-export const clear = (canvas: RefObject<HTMLCanvasElement>) => {
-    if (canvas.current && canvas.current.getContext('2d')) {
-        const context = canvas.current.getContext('2d') as any;
-        const boundedClientRect = canvas.current.getBoundingClientRect();
-        context.clearRect(0, 0, boundedClientRect.width, boundedClientRect.height);
-    }
 };
 
 export const drawPoints2 = (
