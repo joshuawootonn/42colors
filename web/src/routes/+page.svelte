535aec5996321ec7c97e899d3c4ec5b71ad9e029
Joshua Wootonn <joshuawootonn@gmail.com>
2024-07-29 08:09:22 -0400

    feat: add really dumb toggle between modes

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onDestroy, onMount } from 'svelte';
+	import { writable, type Subscriber, type Writable } from 'svelte/store';
 
 	function randomHex() {
 		return '#' + (Math.random() * 1000000000000).toString(16).slice(0, 6);
@@ -8,10 +9,13 @@
 	// const colors = Array.from({ length: 15000 }, () => `#ffffff`);
 	const initialColorOptions = Array.from({ length: 32 }, randomHex);
 
+	type Mode = 'pencil' | 'pan';
+
 	class Canvas {
 		private isDragging = false;
 		private ctx: CanvasRenderingContext2D;
 		private rafId: number = 0;
+		mode: Writable<Mode> = writable<Mode>('pan');
 
 		constructor(private readonly canvas: HTMLCanvasElement) {
 			const context = canvas.getContext('2d');
@@ -98,6 +102,15 @@
 				{ once: true }
 			);
 		}
+
+		changeMode(mode: Mode) {
+			console.log(mode);
+			this.mode.set(mode);
+		}
+
+		subscribeToMode(run: Subscriber<Mode>) {
+			return this.mode.subscribe(run);
+		}
 	}
 
 	class Camera {
@@ -128,6 +141,8 @@
 	onDestroy(() => {
 		canvas?.cleanUp();
 	});
+
+	$: myMode = canvas?.mode;
 </script>
 
 <div class="flex fixed">
@@ -135,6 +150,41 @@
 	{#each initialColorOptions as color}
 		<input type="color" class="aspect-square w-4 h-4 border-0 border-red-700" value={color} />
 	{/each}
+
+	<div>{$myMode}</div>
+	<button on:click={() => canvas?.changeMode('pan')}>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="size-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002"
+			/>
+		</svg>
+	</button>
+
+	<button on:click={() => canvas?.changeMode('pencil')}>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="size-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+			/>
+		</svg>
+	</button>
 </div>
 
 <canvas id="my-house" height="100vh" width="100vw" />
