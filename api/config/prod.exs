063d19d8eefc1e7e0b9fa0d1e8bb9b479b0cf904
Joshua Wootonn <joshuawootonn@gmail.com>
2025-01-27 09:20:23 -0500

    feat: change log level of prod

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -14,7 +14,8 @@ config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Api.Finch
 config :swoosh, local: false
 
 # Do not print debug messages in production
-config :logger, level: :info
+# config :logger, level: :info
+config :logger, :console, format: "[$level] $message\n"
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
