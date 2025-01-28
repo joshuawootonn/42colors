baa0b4b4bfa8b821187d0da2ab9a474db204d393
Joshua Wootonn <joshuawootonn@gmail.com>
2025-01-28 08:03:52 -0500

    feat: add link primitive

---
+++ web/components/link.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import NextLink, { LinkProps } from "next/link";
+
+type Props = Omit<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  keyof LinkProps
+> &
+  LinkProps & {
+    children?: React.ReactNode | undefined;
+  } & React.RefAttributes<HTMLAnchorElement>;
+
+export function Link({ className, ...props }: Props) {
+  return (
+    <NextLink
+      className={cn(
+        "outline-none svg-outline-sm relative no-underline",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
