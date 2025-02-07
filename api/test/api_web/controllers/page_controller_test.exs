6867ed71336809b9f86159013eb6a2da0244e517
Joshua Wootonn <joshuawootonn@gmail.com>
2025-02-07 08:24:06 -0500

    feat: fix existing tests

--- api/test/api_web/controllers/page_controller_test.exs
+++ api/test/api_web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule ApiWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+
+    assert String.contains?(html_response(conn, 200), "What's up brother?!")
   end
 end
