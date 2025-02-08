7824df8b64dea5759f2069a8f6e34a7d8ff97611
Joshua Wootonn <joshuawootonn@gmail.com>
2025-02-08 07:46:51 -0500

    feat: change the test config to point the local port

--- api/config/test.exs
+++ api/config/test.exs
@@ -12,6 +12,7 @@ config :api, Api.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
+  port: 5434,
   database: "api_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
