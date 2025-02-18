6da812804932274a506a498f064b6ede96b27ea7
Joshua Wootonn <joshuawootonn@gmail.com>
2025-02-18 07:29:16 -0500

    feat: update navigation to update search params

--- web/components/navigation.tsx
+++ web/components/navigation.tsx
@@ -1,6 +1,8 @@
 import { Input } from "./ui/input";
 import { useCanvas, useContextCanvasSubscription } from "./use-canvas";
 import { cn } from "@/lib/utils";
+import { ChangeEvent, useCallback } from "react";
+import { useCameraSearchParams } from "./use-camera-search-params";
 
 export function Navigation() {
   const canvas = useCanvas();
@@ -13,6 +15,21 @@ export function Navigation() {
     [],
   );
 
+  const onChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.currentTarget.name === "x") {
+        canvas.camera.x = parseInt(e.currentTarget.value);
+      } else if (e.currentTarget.name === "y") {
+        canvas.camera.y = parseInt(e.currentTarget.value);
+      }
+
+      canvas.emitChange();
+    },
+    [canvas],
+  );
+
+  useCameraSearchParams(cameraX, cameraY);
+
   return (
     <div
       className={cn(
@@ -22,26 +39,22 @@ export function Navigation() {
       <Input
         className="w-20"
         type="number"
+        name="x"
         value={cameraX}
         min={-50500}
         max={50500}
         step={10}
-        onChange={(e) => {
-          canvas.camera.x = parseInt(e.currentTarget.value);
-          canvas.emitChange();
-        }}
+        onChange={onChange}
       />
       <Input
         className="w-20"
         type="number"
+        name="y"
         value={cameraY}
         step={10}
         min={-50500}
         max={50500}
-        onChange={(e) => {
-          canvas.camera.y = parseInt(e.currentTarget.value);
-          canvas.emitChange();
-        }}
+        onChange={onChange}
       />
     </div>
   );
---
+++ web/components/use-camera-search-params.tsx
@@ -0,0 +1,33 @@
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+export const useDebouncedEffect = (
+  effect: (...args: unknown[]) => void,
+  deps: unknown[],
+  delay: number,
+) => {
+  useEffect(() => {
+    const handler = setTimeout(() => effect(), delay);
+
+    return () => clearTimeout(handler);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [...(deps || []), delay]);
+};
+
+export function useCameraSearchParams(x: number, y: number) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useDebouncedEffect(
+    () => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set("x", x.toString());
+      params.set("y", y.toString());
+
+      router.push(`${pathname}?${params.toString()}`);
+    },
+    [pathname, router, searchParams, x, y],
+    300,
+  );
+}
