465846a08e682686405daef30dfa0c3f45be8569
Joshua Wootonn <joshuawootonn@gmail.com>
2025-02-19 08:06:21 -0500

    fix: fix bug in navigation when input is cleared

--- web/components/navigation.tsx
+++ web/components/navigation.tsx
@@ -3,6 +3,9 @@ import { useCanvas, useContextCanvasSubscription } from "./use-canvas";
 import { cn } from "@/lib/utils";
 import { ChangeEvent, useCallback } from "react";
 import { useCameraSearchParams } from "./use-camera-search-params";
+import { z } from "zod";
+
+const numberSchema = z.number();
 
 export function Navigation() {
   const canvas = useCanvas();
@@ -17,10 +20,15 @@ export function Navigation() {
 
   const onChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
+      const parsedNumber = numberSchema.safeParse(
+        parseInt(e.currentTarget.value),
+      );
+      const next = parsedNumber.success ? parsedNumber.data : 0;
+
       if (e.currentTarget.name === "x") {
-        canvas.camera.x = parseInt(e.currentTarget.value);
+        canvas.camera.x = next;
       } else if (e.currentTarget.name === "y") {
-        canvas.camera.y = parseInt(e.currentTarget.value);
+        canvas.camera.y = next;
       }
 
       canvas.emitChange();
