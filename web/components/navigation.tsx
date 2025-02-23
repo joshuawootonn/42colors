b915417ae81f9d760931a60eb06fc3aab299688f
Joshua Wootonn <joshuawootonn@gmail.com>
2025-02-22 21:23:47 -0500

    feat: add an ok home button

--- web/components/navigation.tsx
+++ web/components/navigation.tsx
@@ -44,6 +44,31 @@ export function Navigation() {
         "flex space-x-1.5 justify-center items-center select-none text-xl",
       )}
     >
+      <button
+        onClick={() => {
+          canvas.camera.x = 0;
+          canvas.camera.y = 0;
+          canvas.emitChange();
+        }}
+        className={
+          "flex justify-center items-center h-8 bg-transparent text-base"
+        }
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+          <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+        </svg>
+      </button>
       <Input
         className="w-20"
         type="number"
