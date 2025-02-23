034d99021bf3916a6ddb066ffe88e63471fb5d06
Joshua Wootonn <joshuawootonn@gmail.com>
2025-02-23 09:16:31 -0500

    feat: introduce the PixelCacheOptions struct to enable better testing

--- api/lib/api/pixel_cache.ex
+++ api/lib/api/pixel_cache.ex
@@ -1,23 +1,26 @@
+defmodule Api.PixelCacheOptions do
+  defstruct file_path: Application.compile_env(:api, Api.PixelCache)[:pixel_cache_file_name],
+            canvas_width: Application.compile_env(:api, Api.PixelCache)[:canvas_width],
+            canvas_height: Application.compile_env(:api, Api.PixelCache)[:canvas_height],
+            viewport_diameter: Application.compile_env(:api, Api.PixelCache)[:viewport_diameter]
+end
+
 defmodule Api.PixelCache do
-  def initialize_file() do
-    file_path = Application.get_env(:api, Api.PixelCache)[:pixel_cache_file_name]
-
-    max_x = Application.get_env(:api, Api.PixelCache)[:canvas_width]
-    max_y = Application.get_env(:api, Api.PixelCache)[:canvas_height]
-
-    size = max_x * max_y * 8
+  def initialize_file(options \\ %Api.PixelCacheOptions{}) do
+    size = options.canvas_width * options.canvas_height * 8
     zeros = <<0::size(size)>>
 
-    File.write(file_path, zeros)
+    File.write(options.file_path, zeros)
   end
 
-  defp write_coord(coord, file) do
-    max_x = Application.get_env(:api, Api.PixelCache)[:canvas_width]
-    max_y = Application.get_env(:api, Api.PixelCache)[:canvas_height]
-    negative_offset = trunc(max_x * (max_y / 2) + max_y / 2)
-    offset = trunc(negative_offset + coord.y * max_x + coord.x)
+  defp write_coord(coord, file, options \\ %Api.PixelCacheOptions{}) do
+    canvas_width = options.canvas_width
+    canvas_height = options.canvas_height
 
-    if(abs(coord.x) > max_x / 2 || abs(coord.y) > max_y / 2) do
+    negative_offset = trunc(canvas_width * (canvas_height / 2) + canvas_height / 2)
+    offset = trunc(negative_offset + coord.y * canvas_width + coord.x)
+
+    if(abs(coord.x) > canvas_width / 2 || abs(coord.y) > canvas_height / 2) do
       nil
     else
       case :file.position(file, offset) do
@@ -29,7 +32,7 @@ defmodule Api.PixelCache do
           IO.puts("Error seeking to #{offset} with reason: #{reason}")
 
           IO.puts(
-            "max x: #{max_x},max y: #{max_y}, negative_offset: #{negative_offset}, coord x: #{coord.x}, coord y: #{coord.y}"
+            "canvas_width: #{canvas_width},canvas_height: #{canvas_height}, negative_offset: #{negative_offset}, coord x: #{coord.x}, coord y: #{coord.y}"
           )
       end
 
@@ -44,52 +47,51 @@ defmodule Api.PixelCache do
     end
   end
 
-  def write_coordinates_to_file(coordinates) do
-    file_path = Application.get_env(:api, Api.PixelCache)[:pixel_cache_file_name]
-
+  def write_coordinates_to_file(coordinates, options \\ %Api.PixelCacheOptions{}) do
     # opening in read + write mode prevent this thing from truncating the file
-    file = File.open!(file_path, [:raw, :binary, :read, :write, :delayed_write])
+    file = File.open!(options.file_path, [:raw, :binary, :read, :write, :delayed_write])
 
     Enum.each(coordinates, fn coord -> write_coord(coord, file) end)
     File.close(file)
   end
 
-  def read_sub_section_of_file(point) do
-    file_path = Application.get_env(:api, Api.PixelCache)[:pixel_cache_file_name]
-    max_x = Application.get_env(:api, Api.PixelCache)[:canvas_width]
-    max_y = Application.get_env(:api, Api.PixelCache)[:canvas_height]
-    viewport_diameter = Application.get_env(:api, Api.PixelCache)[:viewport_diameter]
-    half_x = trunc(max_x / 2)
-    half_y = trunc(max_y / 2)
+  def read_sub_section_of_file(point, options \\ %Api.PixelCacheOptions{}) do
+    canvas_width = options.canvas_width
+    canvas_height = options.canvas_height
+    viewport_diameter = options.viewport_diameter
+    half_x = trunc(canvas_width / 2)
+    half_y = trunc(canvas_height / 2)
     half_viewport = trunc(viewport_diameter / 2)
-    negative_offset = trunc(max_x * half_viewport + half_viewport)
+    negative_offset = trunc(canvas_width * half_viewport + half_viewport)
 
     absolute_point = %{x: point.x + half_x, y: point.y + half_y}
-    absolute_position = absolute_point.x + absolute_point.y * max_x
+    absolute_position = absolute_point.x + absolute_point.y * canvas_width
 
     absolute_start_position = absolute_position - negative_offset
 
-    file = File.open!(file_path, [:raw, :binary, :read])
+    file = File.open!(options.file_path, [:raw, :binary, :read])
 
     coords =
       0..viewport_diameter
       |> Enum.to_list()
       |> Enum.reduce([], fn j, acc ->
-        absolute_start_position_of_row = absolute_start_position + max_x * j
+        absolute_start_position_of_row = absolute_start_position + canvas_width * j
         :file.position(file, absolute_start_position_of_row)
 
-        y = Integer.floor_div(absolute_start_position_of_row, max_y) - trunc(max_y / 2)
+        y =
+          Integer.floor_div(absolute_start_position_of_row, canvas_height) -
+            trunc(canvas_height / 2)
 
         # IO.inspect(%{
         #   absolute_position: absolute_position,
         #   negative_offset: negative_offset,
         #   half_viewport: half_viewport,
         #   absolute_start_position: absolute_start_position,
-        #   max_x: max_x,
+        #   canvas_width: canvas_width,
         #   j: j,
         #   y: y,
         #   absolute_start_position_of_row: absolute_start_position_of_row,
-        #   max_y: max_y
+        #   canvas_height: canvas_height
         # })
 
         coords =
@@ -105,8 +107,8 @@ defmodule Api.PixelCache do
                 if byte != 0 do
                   # IO.inspect(i, label: "index")
                   x =
-                    Integer.mod(absolute_start_position_of_row + i, max_x) -
-                      trunc(max_y / 2)
+                    Integer.mod(absolute_start_position_of_row + i, canvas_width) -
+                      trunc(canvas_height / 2)
 
                   # IO.inspect(acc, label: "")
                   # IO.puts("data: #{byte}, index: #{i}, x: #{x}, y: #{y}")
@@ -128,28 +130,27 @@ defmodule Api.PixelCache do
     coords
   end
 
-  def read_sub_section_of_file_as_binary(point) do
-    file_path = Application.get_env(:api, Api.PixelCache)[:pixel_cache_file_name]
-    max_x = Application.get_env(:api, Api.PixelCache)[:canvas_width]
-    max_y = Application.get_env(:api, Api.PixelCache)[:canvas_height]
-    viewport_diameter = Application.get_env(:api, Api.PixelCache)[:viewport_diameter]
-    half_x = trunc(max_x / 2)
-    half_y = trunc(max_y / 2)
+  def read_sub_section_of_file_as_binary(point, options \\ %Api.PixelCacheOptions{}) do
+    canvas_width = options.canvas_width
+    canvas_height = options.canvas_height
+    viewport_diameter = options.viewport_diameter
+    half_x = trunc(canvas_width / 2)
+    half_y = trunc(canvas_height / 2)
     half_viewport = trunc(viewport_diameter / 2)
-    negative_offset = trunc(max_x * half_viewport + half_viewport)
+    negative_offset = trunc(canvas_width * half_viewport + half_viewport)
 
     absolute_point = %{x: point.x + half_x, y: point.y + half_y}
-    absolute_position = absolute_point.x + absolute_point.y * max_x
+    absolute_position = absolute_point.x + absolute_point.y * canvas_width
 
     absolute_start_position = absolute_position - negative_offset
 
-    file = File.open!(file_path, [:raw, :binary, :read])
+    file = File.open!(options.file_path, [:raw, :binary, :read])
 
     matrix =
       0..viewport_diameter
       |> Enum.to_list()
       |> Enum.reduce(<<>>, fn j, acc ->
-        absolute_start_position_of_row = absolute_start_position + max_x * j
+        absolute_start_position_of_row = absolute_start_position + canvas_width * j
         :file.position(file, absolute_start_position_of_row)
 
         # IO.inspect(%{
@@ -157,11 +158,11 @@ defmodule Api.PixelCache do
         #   negative_offset: negative_offset,
         #   half_viewport: half_viewport,
         #   absolute_start_position: absolute_start_position,
-        #   max_x: max_x,
+        #   canvas_width: canvas_width,
         #   j: j,
         #   y: y,
         #   absolute_start_position_of_row: absolute_start_position_of_row,
-        #   max_y: max_y
+        #   canvas_height: canvas_height
         # })
 
         line =
