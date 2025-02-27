95703b427b935876fc76cf935e835af7c6a64792
Joshua Wootonn <joshuawootonn@gmail.com>
2025-02-26 21:16:52 -0500

    feat: change viewport diameter

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -22,5 +22,5 @@ config :logger, :console, format: "[$level] $message\n"
 config :api, Api.PixelCache,
   canvas_height: 10000,
   canvas_width: 10000,
-  viewport_diameter: 2000,
+  viewport_diameter: 400,
   pixel_cache_file_name: "pixel_cache.bin"
--- web/lib/constants.ts
+++ web/lib/constants.ts
@@ -1,3 +1,3 @@
 export const CANVAS_BUFFER = 100;
 export const CANVAS_PIXEL_RATIO = 5;
-export const CHUNK_LENGTH = 2000;
+export const CHUNK_LENGTH = 400;
