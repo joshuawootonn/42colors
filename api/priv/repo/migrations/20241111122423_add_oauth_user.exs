52a916742e5f599533a013f8e6bd6308721d0b49
Joshua Wootonn <joshuawootonn@gmail.com>
2025-02-26 21:51:23 -0500

    chore: fix warning in early migration

--- api/priv/repo/migrations/20241111122423_add_oauth_user.exs
+++ api/priv/repo/migrations/20241111122423_add_oauth_user.exs
@@ -1,6 +1,5 @@
 defmodule Api.Repo.Migrations.AddOauthUser do
   use Ecto.Migration
-  alias Ecto.Migration
 
   def change do
     execute "alter table users alter column hashed_password drop not null"
