93d5e9345ccbad958aa94a30395e63a227f1f4f2
Joshua Wootonn <joshuawootonn@gmail.com>
2025-03-04 07:16:34 -0500

    chore: updated changelog

--- web/app/(marketing)/changelog/component.tsx
+++ web/app/(marketing)/changelog/component.tsx
@@ -26,6 +26,28 @@ export default function Changelog() {
       <hr className="w-full border-t-2 border-black dark:border-white" />
       <ul>
         <li>
+          <DateComponent date={new Date("3/4/2025")} /> — Shipped a new
+          navigation component that accelerates when you hold the controls and
+          has some of the new design patterns I'm thinking of using in the
+          toolbar.
+        </li>
+        <li>
+          <DateComponent date={new Date("2/1/2025")} /> — Experimented a bunch
+          with how to efficiently transfer canvas data. I settled on something
+          that is sending binary data that only includes the colors, they are
+          then rendered based on the offset of where they were fetched from.
+          Since I&apos;m limiting the color palette to 42 colors I am using 1
+          byte for each &quot;pixel&quot; on the screen. This means I can fetch
+          a 400 x 400 tile and it&apos;s at most 16,000 bytes before
+          compression.
+          <br />
+          During this time I also setup file based cache for the entire 10,000 x
+          10,000 canvas so that each request doesn&apos;t hit the database.
+          100,000,000 bytes is only 95 MBs, so I could keep this in memory, but
+          I plan on expanding the canvas to by a factor of 10 so this file cache
+          will eventually be 9.5GBs.
+        </li>{" "}
+        <li>
           <DateComponent date={new Date("1/10/2025")} /> — Started working on
           the project again and created this route interception pattern where
           all routes are modals by default.
