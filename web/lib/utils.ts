40d01bd56d575fa25209e10c3a15f8d059e6a81d
Joshua Wootonn <joshuawootonn@gmail.com>
2025-03-16 08:35:26 -0400

    feat: test if clearing cookies with domain works

--- web/lib/utils.ts
+++ web/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function deleteCookie(name: string) {
-  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+  document.cookie = `${name}=;domain=${process.env.NEXT_PUBLIC_ENV === "production" ? ".42colors.com" : "localhost"}; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
 }
 
 export function roundDown(num: number) {
