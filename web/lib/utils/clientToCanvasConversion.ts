0e2992269dbaca3bba51a07ca947627310f6b745
Joshua Wootonn <joshuawootonn@gmail.com>
2025-03-18 05:33:42 -0400

    feat: update clientToCanvasConversions to use constants

--- web/lib/utils/clientToCanvasConversion.ts
+++ web/lib/utils/clientToCanvasConversion.ts
@@ -1,3 +1,5 @@
+import { CANVAS_PIXEL_RATIO, ZOOM_DEFAULT } from "../constants";
+
 /**
  * @param {number} clientX or clientY - A pixel from the clients viewport
  * @returns {number} x or y - The cooresponding pixel from the canvas
@@ -6,7 +8,7 @@ export function clientToCanvasConversion(
   clientX: number,
   zoom: number,
 ): number {
-  const pixelDensity = zoom / 20;
+  const pixelDensity = zoom / (ZOOM_DEFAULT / CANVAS_PIXEL_RATIO);
   return Math.round(clientX / pixelDensity);
 }
 
@@ -14,6 +16,6 @@ export function canvasToClientConversion(
   canvasX: number,
   zoom: number,
 ): number {
-  const pixelDensity = zoom / 20;
+  const pixelDensity = zoom / (ZOOM_DEFAULT / CANVAS_PIXEL_RATIO);
   return Math.round(canvasX * pixelDensity);
 }
