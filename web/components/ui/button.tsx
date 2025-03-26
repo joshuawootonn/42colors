b33faf34e6ef046abeecb8ed7ef92a0ea803d281
Joshua Wootonn <joshuawootonn@gmail.com>
2025-03-25 21:16:21 -0400

    fix: fix odd type error for how button implements slot

--- web/components/ui/button.tsx
+++ web/components/ui/button.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
@@ -37,8 +36,8 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
+  ({ className, variant, size, ...props }, ref) => {
+    const Comp = "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
