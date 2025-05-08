70df04463f630352959d36c34059561777e1fed1
Joshua Wootonn <joshuawootonn@gmail.com>
2025-05-07 20:53:11 -0500

    feat: create and test chunk util

---
+++ web/lib/utils/chunk.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from "vitest";
+import { chunk } from "./chunk";
+
+describe("chunk", () => {
+  test("happy path", () => {
+    const arr = [1, 2, 3, 4];
+
+    expect(chunk(arr, 2)).toEqual([
+      [1, 2],
+      [3, 4],
+    ]);
+  });
+
+  test("what happens to extra extra", () => {
+    const arr = [1, 2, 3, 4, 5];
+
+    expect(chunk(arr, 2)).toEqual([[1, 2], [3, 4], [5]]);
+  });
+});
---
+++ web/lib/utils/chunk.ts
@@ -0,0 +1,11 @@
+export function chunk<T>(arr: T[], chunkLength: number): T[][] {
+  const chunks: T[][] = [];
+  let i = 0;
+
+  while (i < arr.length) {
+    chunks.push(arr.slice(i, i + chunkLength));
+    i += chunkLength;
+  }
+
+  return chunks;
+}
