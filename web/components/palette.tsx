c10f476d3a66c7d1678921aa803a52e456f83be2
Joshua Wootonn <joshuawootonn@gmail.com>
2025-05-11 08:10:12 -0400

    fix: fix issues with using kebab case in some svgs

--- web/components/palette.tsx
+++ web/components/palette.tsx
@@ -136,9 +136,9 @@ export function Palette() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="1.75"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.75"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <path
                 d="M12 22a1 1 0 0 1 0-20 10 9 0 0 1 10 9 5 5 0 0 1-5 5h-2.25a1.75 1.75 0 0 0-1.4 2.8l.3.4a1.75 1.75 0 0 1-1.4 2.8z"
