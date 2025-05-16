d471112b85d8f0245c9f69a3636f5d390b346055
Joshua Wootonn <joshuawootonn@gmail.com>
2025-05-15 21:10:52 -0400

    feat: create net-new-coord for finding the new coords from two lists

---
+++ web/lib/utils/net-new-coords.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, test } from "vitest";
+import { coordSort, Pixel, pixelSchema } from "../coord";
+import { newNewCoords } from "./net-new-coords";
+
+describe("net new pixels", () => {
+  test("happy path", () => {
+    const arr: Pixel[] = [
+      pixelSchema.parse({ x: 1, y: 1, colorRef: 1 }),
+      pixelSchema.parse({ x: 1, y: 2, colorRef: 0 }),
+    ];
+
+    const next: Pixel[] = [
+      pixelSchema.parse({ x: 1, y: 2, colorRef: 0 }),
+      pixelSchema.parse({ x: 2, y: 1, colorRef: 1 }),
+      pixelSchema.parse({ x: 2, y: 2, colorRef: 1 }),
+    ];
+
+    expect(newNewCoords(arr, next).sort(coordSort)).toEqual(
+      [
+        pixelSchema.parse({ x: 2, y: 1, colorRef: 1 }),
+        pixelSchema.parse({ x: 2, y: 2, colorRef: 1 }),
+      ].sort(coordSort),
+    );
+  });
+});
---
+++ web/lib/utils/net-new-coords.ts
@@ -0,0 +1,30 @@
+import { Coord } from "../coord";
+
+export function newNewCoords<T extends Coord>(
+  coords: T[],
+  newCoords: T[],
+): T[] {
+  const cache: Record<string, boolean> = {};
+
+  const unique: T[] = [];
+  const netNew: T[] = [];
+  for (let i = coords.length - 1; i >= 0; i--) {
+    const coord = coords[i];
+    const coordHash = `${coord.x}-${coord.y}`;
+
+    if (cache[coordHash] == null) {
+      unique.unshift(coord);
+      cache[coordHash] = true;
+    }
+  }
+
+  for (let i = 0; i < newCoords.length; i++) {
+    const coord = newCoords[i];
+    const coordHash = `${coord.x}-${coord.y}`;
+    if (cache[coordHash] == null) {
+      netNew.push(coord);
+    }
+  }
+
+  return netNew;
+}
