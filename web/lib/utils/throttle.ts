e2d735666f879f19fa8985699e6deca08cf484c8
Joshua Wootonn <joshuawootonn@gmail.com>
2025-05-26 15:41:07 -0400

    chore: add throttle function

---
+++ web/lib/utils/throttle.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
+
+export function throttle(func: Function, delay: number): Function {
+  let wait = false;
+
+  return (...args: unknown[]) => {
+    if (wait) {
+      return;
+    }
+
+    func(...args);
+    wait = true;
+    setTimeout(() => {
+      wait = false;
+    }, delay);
+  };
+}
