3ef0a9b557be20c8923fd60eec06c62f7382cc8d
Joshua Wootonn <joshuawootonn@gmail.com>
2025-06-16 20:37:55 -0400

    chore: update runtime configuration to use mailpace

--- api/config/runtime.exs
+++ api/config/runtime.exs
@@ -112,10 +112,10 @@ if config_env() == :prod do
   # Also, you may need to configure the Swoosh API client of your choice if you
   # are not using SMTP. Here is an example of the configuration:
   #
-  #     config :api, Api.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
+  config :api, Api.Mailer,
+    adapter: Swoosh.Adapters.MailPace,
+    api_key: System.get_env("MAILPACE_API_KEY")
+
   #
   # For this example you need include a HTTP client required by Swoosh API client.
   # Swoosh supports Hackney and Finch out of the box:
