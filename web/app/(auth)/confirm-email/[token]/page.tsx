342d48921684fab62e740394db9689fc8c606404
Joshua Wootonn <joshuawootonn@gmail.com>
2025-06-16 21:25:05 -0400

    feat: add user confirmation route

---
+++ web/app/(auth)/confirm-email/[token]/page.tsx
@@ -0,0 +1,14 @@
+import authService from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ token: string }>;
+}) {
+  const token = (await params).token;
+
+  await authService.confirmEmail(token);
+
+  redirect("/");
+}
