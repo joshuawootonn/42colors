c9d5cac64dbc84aca876d9ed46777e98d260ec9d
Joshua Wootonn <joshuawootonn@gmail.com>
2025-06-29 17:47:09 -0400

    feat: only show buttons to alter claim when one is selected

--- web/lib/tools/claimer-panel.tsx
+++ web/lib/tools/claimer-panel.tsx
@@ -18,6 +18,7 @@ import {
 import { IconButton } from "@/components/ui/icon-button";
 import { X } from "@/components/icons/x";
 import * as Tooltip from "@/components/ui/tooltip";
+import { useMemo } from "react";
 
 export function ClaimerPanel() {
   const activeAction = useSelector(
@@ -62,6 +63,8 @@ export function ClaimerPanel() {
     },
   });
 
+  const selectedPlot = useMemo(() => plots?.find((plot) => plot.id === selectedPlotId), [plots, selectedPlotId]);
+
   return (
     <div className="flex-grow flex flex-row items-start justify-start">
       <DropdownMenu>
@@ -69,8 +72,8 @@ export function ClaimerPanel() {
           <Button variant="link" className="">
             {activeAction?.type === "claimer-active" ? (
               "New Claim"
-            ) : selectedPlotId ? (
-              plots?.find((plot) => plot.id === selectedPlotId)?.name
+            ) : selectedPlot ? (
+              selectedPlot.name
             ) : (
               <>
                 Select Plot{" "}
@@ -120,7 +123,7 @@ export function ClaimerPanel() {
             <X />
           </IconButton>
         </>
-      ) : (
+      ) : selectedPlot ? (
         <>
           <Tooltip.Provider>
             <Tooltip.Root>
@@ -151,12 +154,8 @@ export function ClaimerPanel() {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                     >
-                      <path d="M13 7 8.7 2.7a2.41 2.41 0 0 0-3.4 0L2.7 5.3a2.41 2.41 0 0 0 0 3.4L7 13" />
-                      <path d="m8 6 2-2" />
-                      <path d="m18 16 2-2" />
-                      <path d="m17 11 4.3 4.3c.94.94.94 2.46 0 3.4l-2.6 2.6c-.94.94-2.46.94-3.4 0L11 17" />
-                      <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
-                      <path d="m15 5 4 4" />
+                      <path d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z" />
+                      <circle cx="7.5" cy="7.5" r=".5" fill="currentColor" />
                     </svg>
                   </IconButton>
                 }
@@ -212,7 +211,7 @@ export function ClaimerPanel() {
             </Tooltip.Root>
           </Tooltip.Provider>
         </>
-      )}
+      ) : null}
     </div>
   );
 }
