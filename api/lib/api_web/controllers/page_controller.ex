e12c16bc442ac803bc8fdc6ec778068a8be6d1e5
Joshua Wootonn <joshuawootonn@gmail.com>
2025-07-05 09:52:40 -0400

    feat: update page responses to use plain text content type

--- api/lib/api_web/controllers/page_controller.ex
+++ api/lib/api_web/controllers/page_controller.ex
@@ -2,12 +2,14 @@ defmodule ApiWeb.PageController do
   use ApiWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    send_resp(conn, 200, "What's up brother!")
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "What's up brother?!")
   end
 
   def health_check(conn, _params) do
-    send_resp(conn, 200, "We are up and running!")
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "We are up and running!")
   end
 end
--- api/test/api_web/controllers/page_controller_test.exs
+++ api/test/api_web/controllers/page_controller_test.exs
@@ -4,6 +4,12 @@ defmodule ApiWeb.PageControllerTest do
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
 
-    assert String.contains?(html_response(conn, 200), "What's up brother?!")
+    assert String.contains?(text_response(conn, 200), "What's up brother?!")
+  end
+
+  test "GET /up", %{conn: conn} do
+    conn = get(conn, ~p"/up")
+
+    assert String.contains?(text_response(conn, 200), "We are up and running!")
   end
 end
