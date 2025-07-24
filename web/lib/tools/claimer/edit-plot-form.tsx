d7f806b1801d02f7d90e37da9d50f3db84074f43
Joshua Wootonn <joshuawootonn@gmail.com>
2025-07-23 22:05:16 -0500

    fix: client side issues button inside button in the claimer forms

--- web/lib/tools/claimer/edit-plot-form.tsx
+++ web/lib/tools/claimer/edit-plot-form.tsx
@@ -117,29 +117,34 @@ export function EditPlotForm({ plot }: EditPlotFormProps) {
             <Tooltip.Root>
                 <Tooltip.Trigger
                     render={
-                        <PopoverTrigger>
-                            <IconButton className="text-black -translate-x-[1px]">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="32"
-                                    height="32"
-                                    viewBox="-4 -4 32 32"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth="1.5"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
+                        <PopoverTrigger
+                            render={(props) => (
+                                <IconButton
+                                    className="text-black -translate-x-[1px]"
+                                    {...props}
                                 >
-                                    <path d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z" />
-                                    <circle
-                                        cx="7.5"
-                                        cy="7.5"
-                                        r=".5"
-                                        fill="currentColor"
-                                    />
-                                </svg>
-                            </IconButton>
-                        </PopoverTrigger>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="32"
+                                        height="32"
+                                        viewBox="-4 -4 32 32"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="1.5"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    >
+                                        <path d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z" />
+                                        <circle
+                                            cx="7.5"
+                                            cy="7.5"
+                                            r=".5"
+                                            fill="currentColor"
+                                        />
+                                    </svg>
+                                </IconButton>
+                            )}
+                        />
                     }
                 />
                 <Tooltip.Portal>
