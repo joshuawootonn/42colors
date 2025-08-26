4d6de1107fa5cdd05df08ff89333157734858784
Joshua Wootonn <joshuawootonn@gmail.com>
2025-08-25 22:02:21 -0500

    feat: add squirrel stats

--- web/components/fathom.tsx
+++ web/components/fathom.tsx
@@ -2,19 +2,26 @@
 
 import { load, trackPageview } from 'fathom-client';
 
-import { Suspense, useEffect } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 import { usePathname, useSearchParams } from 'next/navigation';
+
+import { SquirrelStats } from '@squirrel-stats/client';
 
 function TrackPageView() {
     const pathname = usePathname();
     const searchParams = useSearchParams();
+    const [squirrelStatsClient, setSquirrelStatsClient] =
+        useState<SquirrelStats | null>(null);
 
     // Load the Fathom script on mount
     useEffect(() => {
         load(process.env.NEXT_PUBLIC_FATHOM_ID ?? '', {
             url: process.env.NEXT_PUBLIC_FATHOM_URL,
         });
+        setSquirrelStatsClient(
+            new SquirrelStats(process.env.NEXT_PUBLIC_SQUIRREL_STATS_ID!),
+        );
     }, []);
 
     // Record a pageview when route changes
@@ -25,7 +32,8 @@ function TrackPageView() {
             url: pathname + searchParams?.toString(),
             referrer: document.referrer,
         });
-    }, [pathname, searchParams]);
+        squirrelStatsClient?.trackPageview();
+    }, [pathname, searchParams, squirrelStatsClient]);
 
     return null;
 }
