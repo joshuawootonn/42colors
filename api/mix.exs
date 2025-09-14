e31355983ae14f0b7f746d737ac2f02369df1884
Joshua Wootonn <joshuawootonn@gmail.com>
2025-09-14 08:46:05 -0500

    chore: update all deps

--- api/mix.exs
+++ api/mix.exs
@@ -68,7 +68,7 @@ defmodule Api.MixProject do
       {:cors_plug, "~> 3.0"},
       {:elixir_auth_google, "~> 1.6.9"},
       {:protobuf, "~> 0.14.0"},
-      {:tidewave, "~> 0.1", only: :dev},
+      {:tidewave, "~> 0.5", only: :dev},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
