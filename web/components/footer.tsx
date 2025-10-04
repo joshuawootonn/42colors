d453eca6459b823fe214e7294929ebb335fe5e04
Joshua Wootonn <joshuawootonn@gmail.com>
2025-10-04 12:35:59 -0500

    feat: improve triple dot menu and rearrange footer

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -32,8 +32,85 @@ export function Footer() {
     return (
         <footer className="text-md fixed bottom-3 left-3 flex w-min flex-col items-center justify-between font-medium text-primary sm:flex-row">
             <div className="mb-3 flex items-center justify-between space-x-3 sm:mb-0">
+                {user != null ? (
+                    <>
+                        <LogsPopover anchor={ref}>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger ref={ref}>
+                                    <span className="whitespace-nowrap">
+                                        {user.email} (<BalanceDiff />)
+                                    </span>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    <PopoverTrigger
+                                        nativeButton={false}
+                                        render={(props) => (
+                                            <DropdownMenuItem {...props}>
+                                                logs
+                                            </DropdownMenuItem>
+                                        )}
+                                    />
+                                    <DropdownMenuItem
+                                        onClick={authService.logout}
+                                    >
+                                        logout
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </LogsPopover>
+                    </>
+                ) : (
+                    <>
+                        <Link
+                            href={{
+                                pathname: '/login',
+                                query: searchParams.toString(),
+                            }}
+                        >
+                            login
+                        </Link>
+                        <div>or</div>
+                        <Link
+                            href={{
+                                pathname: '/signup',
+                                query: searchParams.toString(),
+                            }}
+                        >
+                            signup
+                        </Link>
+                    </>
+                )}
+
+                <div>/</div>
+
+                <PlotsPopover>
+                    <PopoverTrigger
+                        render={(props) => (
+                            <button
+                                {...props}
+                                className="svg-outline-sm relative no-underline"
+                            >
+                                plots
+                            </button>
+                        )}
+                    />
+                </PlotsPopover>
+                <div>/</div>
+
                 <DropdownMenu>
-                    <DropdownMenuTrigger>...</DropdownMenuTrigger>
+                    <DropdownMenuTrigger aria-label="Open general menu">
+                        <svg
+                            width="20"
+                            height="20"
+                            viewBox="0 0 16 16"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <circle cx="4" cy="8" r="1" fill="currentColor" />
+                            <circle cx="8" cy="8" r="1" fill="currentColor" />
+                            <circle cx="12" cy="8" r="1" fill="currentColor" />
+                        </svg>
+                    </DropdownMenuTrigger>
                     <DropdownMenuContent>
                         <DropdownMenuItem
                             render={(props) => (
@@ -101,68 +178,6 @@ export function Footer() {
                         </DropdownMenuSubRoot>
                     </DropdownMenuContent>
                 </DropdownMenu>
-                <div>/</div>
-                <PlotsPopover>
-                    <PopoverTrigger
-                        render={(props) => (
-                            <button
-                                {...props}
-                                className="svg-outline-sm relative no-underline"
-                            >
-                                plots
-                            </button>
-                        )}
-                    />
-                </PlotsPopover>
-                <div>/</div>
-                {user != null ? (
-                    <>
-                        <LogsPopover anchor={ref}>
-                            <DropdownMenu>
-                                <DropdownMenuTrigger ref={ref}>
-                                    <span className="whitespace-nowrap">
-                                        {user.email} (<BalanceDiff />)
-                                    </span>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent>
-                                    <PopoverTrigger
-                                        nativeButton={false}
-                                        render={(props) => (
-                                            <DropdownMenuItem {...props}>
-                                                logs
-                                            </DropdownMenuItem>
-                                        )}
-                                    />
-                                    <DropdownMenuItem
-                                        onClick={authService.logout}
-                                    >
-                                        logout
-                                    </DropdownMenuItem>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
-                        </LogsPopover>
-                    </>
-                ) : (
-                    <>
-                        <Link
-                            href={{
-                                pathname: '/login',
-                                query: searchParams.toString(),
-                            }}
-                        >
-                            login
-                        </Link>
-                        <div>or</div>
-                        <Link
-                            href={{
-                                pathname: '/signup',
-                                query: searchParams.toString(),
-                            }}
-                        >
-                            signup
-                        </Link>
-                    </>
-                )}
             </div>
         </footer>
     );
