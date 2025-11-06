df3d52afd8f7729b31cb09ae17fde11559bcbfe2
Joshua Wootonn <joshuawootonn@gmail.com>
2025-11-05 19:34:50 -0600

    chore: remove squirrel stats

--- web/components/fathom.tsx
+++ web/components/fathom.tsx
@@ -2,26 +2,19 @@
 
 import { load, trackPageview } from 'fathom-client';
 
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense, useEffect } from 'react';
 
 import { usePathname, useSearchParams } from 'next/navigation';
-
-import { SquirrelStats } from '@squirrel-stats/client';
 
 function TrackPageView() {
     const pathname = usePathname();
     const searchParams = useSearchParams();
-    const [squirrelStatsClient, setSquirrelStatsClient] =
-        useState<SquirrelStats | null>(null);
 
     // Load the Fathom script on mount
     useEffect(() => {
         load(process.env.NEXT_PUBLIC_FATHOM_ID ?? '', {
             url: process.env.NEXT_PUBLIC_FATHOM_URL,
         });
-        setSquirrelStatsClient(
-            new SquirrelStats(process.env.NEXT_PUBLIC_SQUIRREL_STATS_ID!),
-        );
     }, []);
 
     // Record a pageview when route changes
@@ -32,8 +25,7 @@ function TrackPageView() {
             url: pathname + searchParams?.toString(),
             referrer: document.referrer,
         });
-        squirrelStatsClient?.trackPageview();
-    }, [pathname, searchParams, squirrelStatsClient]);
+    }, [pathname, searchParams]);
 
     return null;
 }
