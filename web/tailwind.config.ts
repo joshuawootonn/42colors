65910bc2154618732944387706124ac7c7e7e1ea
Joshua Wootonn <joshuawootonn@gmail.com>
2025-11-23 21:59:45 -0600

    feat: create error color

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -310,6 +310,10 @@ export default {
                     DEFAULT: 'hsl(var(--destructive))',
                     foreground: 'hsl(var(--destructive-foreground))',
                 },
+                success: {
+                    DEFAULT: 'hsl(var(--success))',
+                    foreground: 'hsl(var(--success-foreground))',
+                },
                 border: 'hsl(var(--border))',
                 input: 'hsl(var(--input))',
                 ring: 'hsl(var(--ring))',
