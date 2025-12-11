d48bf6bc219a48d3eff40bbf8dcf9def4a7fbea8
Joshua Wootonn <joshuawootonn@gmail.com>
2025-12-10 19:50:05 -0600

    feat: enable os stats in elixir dashboard

--- api/mix.exs
+++ api/mix.exs
@@ -22,7 +22,7 @@ defmodule Api.MixProject do
   def application do
     [
       mod: {Api.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
