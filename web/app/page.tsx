192320b4b87b1d8b99b6bb02883ca2962a9434be
Joshua Wootonn <joshuawootonn@gmail.com>
2025-12-14 19:52:40 -0600

    feat: don't prevent pointer event from hitting the canvas between the footer and nav

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -192,11 +192,11 @@ export default function Page() {
         <Toolbar />
       </div>
 
-      <div className="fixed w-full overflow-hidden inset-x-0 bottom-3 px-3 flex flex-col justify-between items-end gap-2 sm:flex-row sm:items-end sm:justify-between sm:px-3">
-        <div className="order-2 sm:order-1">
+      <div className="pointer-events-none fixed w-full overflow-hidden inset-x-0 bottom-3 px-3 flex flex-col justify-between items-end gap-2 sm:flex-row sm:items-end sm:justify-between sm:px-3">
+        <div className="pointer-events-auto order-2 sm:order-1">
           <Footer />
         </div>
-        <div className="order-1 sm:order-2 min-w-40">
+        <div className="pointer-events-auto order-1 sm:order-2 min-w-40">
           <Navigation />
         </div>
       </div>
